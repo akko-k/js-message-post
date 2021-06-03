485d515f9c86de16475512a409e77ce965409d88
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-06-03 23:08:27 +0900

    ストップウォッチ 変数を準備

---
+++ stop_watch/script.js
@@ -0,0 +1,6 @@
+let seconds = 0;
+let timer;
+
+const display = document.getElementById("display");
+const starsStopButton = document.getElementById("start-stop");
+const resetButton = document.getElementById("reset");
